25e32358180d21b2d77cf940d846d0947476e77a
sujal sevkani <sujalsevkani15@email.com>
2026-01-12 12:43:09 +0530

    folder structure add successfully

---
+++ template.sh
@@ -0,0 +1,18 @@
+#creating project directories
+
+mkdir -p src
+mkdir -p research
+
+# create files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py 
+touch research/trails.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Project structure created successfully."
